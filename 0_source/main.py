2d13ccac03d03d4fae98cfdedccce28dd7bf7fa1
Damien MARGUERITE <damien@super-lucette.localdomain>
2018-05-29 21:44:50 +0200

    Get an array with all loans books is working

--- 0_source/main.py
+++ 0_source/main.py
@@ -2,20 +2,22 @@
 
 from bs4 import BeautifulSoup
 
-print ('Hello World !')
+print('Crosne library Alert !!')
 
 #Open buffer to get all html code
 fileBuffer = open('../3_others/webDump.html', 'r')
 soup = BeautifulSoup(fileBuffer, "html.parser")
-
+data = []
 #Find all loans history table
-allLoansHistory = soup.find_all('table', "tablesorter loans-history")
+table = soup.find('table', "tablesorter loans-history")
+table_body = table.find('tbody')
 
-for tbody in allLoansHistory:
-    for tr in tbody:
-        for td in tr:
-            i=+1
-            print(i)
-            if(i==7):
-                print(td.get_text())
+rows = table_body.find_all('tr')
+
+for row in rows:
+    cols = row.find_all('td')
+    cols = [ele.text.strip() for ele in cols]
+    data.append([ele for ele in cols if ele])
+
+print(data)
 
